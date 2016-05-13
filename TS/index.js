92d4fc3698914e17606d2d25fba4b751caf865be
Ravi Arora <aroraravi87@gmail.com>
2016-05-13 20:17:08 +0530

    index.js file changes

--- TS/index.js
+++ TS/index.js
@@ -1,11 +1,44 @@
-var Startup = (function () {
-    function Startup() {
+var CustomerCollection = (function () {
+    function CustomerCollection() {
+        this.items = [];
     }
-    Startup.main = function () {
-        console.log("hello world");
-        return 1;
+    CustomerCollection.prototype.count = function () {
+        return this.items.length;
     };
-    return Startup;
+    CustomerCollection.prototype.Add = function (item) {
+        this.items.push(item);
+    };
+    return CustomerCollection;
 })();
 
-Startup.main();
+var Program = (function () {
+    function Program(options) {
+        this.collection = new CustomerCollection();
+        this.CustName = options.CustName;
+        this.CustPhNo = options.CustPhNo;
+        this.CustAddress = options.CustAddress;
+        this.CustEmail = options.CustEmail;
+    }
+    Program.prototype.GetDetail = function (customer) {
+        console.log(this.collection.count());
+        if (this.collection.count() > 0) {
+            console.log(this.collection.count());
+            for (var cust in this.collection) {
+                return "<div class='container'><b>Customer Name is : </b>" + cust.CustName + "</br><b>PhoneNo is : </b>" + cust.CustPhNo + "</br/><b>EmailID is : </b>" + cust.CustEmail + "</br/><b>CustAddress is : </b>" + cust.CustAddress + "</div>";
+            }
+        }
+        return "<div class='container'><b>Customer Name is : </b>" + customer.CustName + "</br><b>PhoneNo is : </b>" + customer.CustPhNo + "</br/><b>EmailID is : </b>" + customer.CustEmail + "</br/><b>CustAddress is : </b>" + customer.CustAddress + "</div>";
+    };
+
+    Program.prototype.SaveCustomer = function (objcustomer) {
+        // document.getElementById("grCustomer").innerHTML=objcustomer.GetDetail(objcustomer);
+        console.log(this.collection.count());
+        this.collection.Add(objcustomer);
+        console.log(this.collection.count());
+        return true;
+    };
+    Program.prototype.HelloMethod = function () {
+        alert("Hello Method Called");
+    };
+    return Program;
+})();
--- TS/index.ts
+++ TS/index.ts
@@ -1,8 +1,77 @@
-class Startup {
-    public static main(): number {
-        console.log("hello world");
-        return 1;
+  
+  interface ICustomer {
+    CustName:string;
+    CustPhNo:string;
+    CustEmail:string;
+    CustID:number;
+    CustAddress:string;
+   
+  
+}
+
+interface ICustomerMethods extends ICustomer {
+    SaveCustomer(customer:Program):boolean;
+    HelloMethod():void;
+}
+
+class CustomerCollection{
+    public items:Program[]=[];
+    public count():number{
+        return this.items.length
+        }
+        public Add(item:Program)
+        {
+            this.items.push(item);
+        }
+}
+
+class Program implements ICustomerMethods
+{
+    CustName:string;
+    CustPhNo:string;
+    CustEmail:string;
+    CustID:number;
+    CustAddress:string;
+    collection:CustomerCollection=new CustomerCollection();
+    constructor(options:ICustomer){
+        this.CustName=options.CustName;
+        this.CustPhNo=options.CustPhNo;
+        this.CustAddress=options.CustAddress;
+        this.CustEmail=options.CustEmail; 
+        
+       }
+    
+    public GetDetail(customer:Program):string
+    {
+     console.log(this.collection.count());
+       if(this.collection.count()>0)
+        {
+        console.log(this.collection.count());
+        for (var cust in this.collection) // for acts as a foreach
+        { 
+           return "<div class='container'><b>Customer Name is : </b>"+cust.CustName+"</br><b>PhoneNo is : </b>"+cust.CustPhNo+"</br/><b>EmailID is : </b>"+cust.CustEmail+"</br/><b>CustAddress is : </b>"+cust.CustAddress+"</div>";
+        }
+        }
+        return "<div class='container'><b>Customer Name is : </b>"+customer.CustName+"</br><b>PhoneNo is : </b>"+customer.CustPhNo+"</br/><b>EmailID is : </b>"+customer.CustEmail+"</br/><b>CustAddress is : </b>"+customer.CustAddress+"</div>";
+    }
+    
+    public SaveCustomer(objcustomer:Program):boolean
+    {
+       // document.getElementById("grCustomer").innerHTML=objcustomer.GetDetail(objcustomer);
+     
+     
+      console.log(this.collection.count());
+       this.collection.Add(objcustomer);
+       console.log(this.collection.count());
+        return true;
+    }
+    public HelloMethod():void
+    {
+        alert("Hello Method Called");
+        
     }
 }
 
-Startup.main();
+
+
+
