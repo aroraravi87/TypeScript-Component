8363665ad52d392313bb8f3811410fb081b59009
Ravi Arora <aroraravi87@gmail.com>
2016-05-26 17:31:43 +0530

    modified index.js

--- TS/index.js
+++ TS/index.js
@@ -1,9 +1,63 @@
+var Employeeodel = (function () {
+    function Employeeodel() {
+    }
+    Object.defineProperty(Employeeodel.prototype, "BirthYear", {
+        get: function () {
+            return this._birthYear;
+        },
+        set: function (BY) {
+            this._birthYear = BY;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Employeeodel.prototype, "Mobile", {
+        get: function () {
+            return this._mobile;
+        },
+        set: function (mobile) {
+            this._mobile = mobile;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return Employeeodel;
+})();
+
+//    abstract class SalaryCls
+//     {
+//        private _amount:number;
+//        constructor(amount:number){
+//            this._amount=amount;
+//        }
+//        CalculateBasic(Amount:number):number{return 0;}
+//       abstract CalculateSalary(Amount:number):number;
+//     }
+var Employee = (function () {
+    function Employee(firstName, lastName, year, mobile) {
+        this._firstName = firstName;
+        this._lastName = lastName;
+        this.BirthYear = year;
+        this.Mobile = mobile;
+    }
+    Employee.prototype.CalculateAge = function (CY, BY) {
+        return (CY - BY);
+    };
+
+    Employee.prototype.GetFullName = function () {
+        document.writeln("FullName is : " + this._firstName + " " + this._lastName);
+        document.writeln("<br\> Age is : " + this.CalculateAge(2016, this.BirthYear));
+        document.writeln("<br\> Contact No is : " + this.Mobile);
+    };
+    return Employee;
+})();
+
 var Startup = (function () {
     function Startup() {
     }
     Startup.main = function () {
-        console.log("hello world");
-        return 1;
+        var objEmployee = new Employee('John', 'Smith', 1970, +91990099000);
+        objEmployee.GetFullName();
     };
     return Startup;
 })();
--- TS/index.ts
+++ TS/index.ts
@@ -1,8 +1,81 @@
-class Startup {
-    public static main(): number {
-        console.log("hello world");
-        return 1;
+        interface IEmployee
+        {
+           // private _birthYear:number;
+            //private _mobile:number;
+            
+            CalculateAge(CY:number,BY:number):number;
+        }
+
+     class Employeeodel
+        {
+            private _birthYear:number;
+            private _mobile:number;
+            
+            get BirthYear():number{
+                return this._birthYear;
+            }
+            set BirthYear(BY:number){
+                this._birthYear=BY;
+            }
+            get Mobile():number{
+                return this._mobile;
+            }
+            set Mobile(mobile:number){
+                this._mobile=mobile;
+            }
+        }
+
+       abstract class SalaryCls{
+           private _amount:number;
+           
+           constructor(amount:number){
+               this._amount=amount;
+           }
+           CalculateBasic(Amount:number):number{return 0;}    
+       
+          abstract CalculateSalary(Amount:number):number;    
+          
+        }
+
+
+  class Employee implements IEmployee{
+    
+    private _firstName:string;
+    private _lastName:string;
+    BirthYear:number;
+    Mobile:number;
+    Age:number;
+    
+  constructor(firstName:string,lastName:string,year:number,mobile:number)
+    {
+        this._firstName=firstName;
+        this._lastName= lastName;
+        this.BirthYear=year;
+        this.Mobile=mobile;
+  }
+  
+  CalculateAge(CY:number,BY:number):number
+  {
+     return (CY-BY);
+  }
+    
+    GetFullName():void
+    {
+        document.writeln("FullName is : "+this._firstName +" "+this._lastName );
+        document.writeln("<br\> Age is : "+ this.CalculateAge(2016,this.BirthYear) );
+        document.writeln("<br\> Contact No is : "+this.Mobile);
+    }
+}
+
+
+
+ class Startup {
+    public static main(): any {
+         var objEmployee:Employee=new Employee('John','Smith',1970,+91990099000);
+         objEmployee.GetFullName();
+       
     }
 }
 
 Startup.main();
+ 
